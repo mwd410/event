888b056f9e9b71b6665442a86ad4b30d23ad1f48
Matt Deady <mwd410@comcast.net>
2014-01-30 13:52:31 -0500

    refactoring config

--- config/config.js
+++ config/config.js
@@ -1,13 +1,39 @@
 'use strict';
 
-module.exports = {
-    all : {
-        port : 3000
-    },
-    prod : {
+var _ = require('lodash'),
+    walkSync = require('utils').walkSync,
+    deepApply = require('utils').deepApply,
+    path = require('path');
 
-    },
-    dev : {
 
+
+module.exports = function(env) {
+
+    var config = {},
+        fileNameRegex = /^(\w+)\.js$/;
+
+    var walkResult = walkSync(__dirname, function(err, filePath, stat) {
+
+        if (stat.isDirectory() && env !== path.basename(filePath) ||
+            !stat.isDirectory() && filePath === __filename ||
+            !fileNameRegex.test(path.basename(filePath))) {
+
+            return false;
+        }
+    });
+
+    function applyConfig(fn, file) {
+        if (typeof fn !== 'function') {
+            return;
+        }
+        var section = file.match(fileNameRegex)[1];
+        config[section] = deepApply(config[section] || {}, fn(true));
     }
+
+    _.forEach(walkResult, applyConfig);
+    if (walkResult[env]) {
+        _.forEach(walkResult[env], applyConfig);
+    }
+
+    return config;
 };
