284d9f1e038ad5ce4aaf9f30df0b817f35de06b9
Matt Deady <mwd410@comcast.net>
2014-02-03 16:05:38 -0500

    Added define service defineNamespace

--- middleware/apiResponse.js
+++ middleware/apiResponse.js
@@ -4,15 +4,20 @@ module.exports = function(req, res, next) {
 
     var api = res.api = {};
 
+    api.statusCode = 200;
+
     api.success = function(data) {
-        res.send({
+        res.send(api.statusCode, {
             success : true,
             data    : data
         });
     };
 
     api.error = function(error) {
-        res.send(400, {
+        if (api.statusCode === 200) {
+            api.statusCode = 500;
+        }
+        res.send(api.statusCode, {
             success : false,
             error   : error
         });
